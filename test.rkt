ae22147c9c626ea17ec0aa65039cb09f7e6de809
Michael W <gcr@sneakygcr.net>
2012-05-27 12:52:02 -0600

    change test

--- test.rkt
+++ test.rkt
@@ -1,16 +1,14 @@
-#lang nonexistent
-(require (planet gcr/bonk))
+#lang racket
+(require (planet gcr/riot))
 
-(define queue (queue "localhost" 6344))
+(define cloud (connect-to-queue "localhost" 2355))
 
 (define workunits
   (for/list ([i (in-range 10)])
-    (queue (workunit-λ
-            (do-stuff i j k)))))
+    (do-work cloud
+      (+ i 5))))
 
-(for/list ([p workunits]) (workunit-result p))
-
-(workunit-status (first p))
+(for/list ([p workunits]) (wait-until-done cloud p))
 
 ;; Queue would be responsible for
 ;; - generating a UUID for this director node
@@ -20,9 +18,7 @@
 ;; - Saving workunit results from clients and relaying them
 ;;   back to the server
 ;; - Serializing (require racket/serialize) parameters and results
-;;   using fasl
 
 ;; Maybe use web-server/lang/serial-lambda ?
-
 
 ;; racket -p gcr/bonk/client --queue localhost:6344 --file test.rkt
